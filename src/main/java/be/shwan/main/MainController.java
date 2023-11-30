706dec40174d4f507c350de283969eb3992e10d3
IIIXwon <IIIXwon@gmail.com>
2023-11-30 18:23:00 +0900

    테스트 페이지 추가

---
+++ src/main/java/be/shwan/main/MainController.java
@@ -0,0 +1,13 @@
+package be.shwan.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String mainPage() {
+        return "index";
+    }
+
+}
