e621d9f97faa2d55224c9e93f13bebdfa9604767
IIIXwon <IIIXwon@gmail.com>
2023-12-05 16:46:11 +0900

    modelmapper 제거

--- src/main/java/be/shwan/config/AppConfig.java
+++ src/main/java/be/shwan/config/AppConfig.java
@@ -1,13 +1,8 @@
 package be.shwan.config;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+
 }
