23eb7d2d79b32332a5d8d4ed4b4f894d36f37783
IIIXwon <IIIXwon@gmail.com>
2023-12-07 11:10:13 +0900

    event 속성 추가

--- src/main/java/be/shwan/event/domain/Event.java
+++ src/main/java/be/shwan/event/domain/Event.java
@@ -1,6 +1,7 @@
 package be.shwan.event.domain;
 
 import be.shwan.account.domain.Account;
+import be.shwan.enrollment.domain.Enrollment;
 import be.shwan.study.domain.Study;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
@@ -8,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @EqualsAndHashCode(of = "id")
@@ -24,18 +26,29 @@ public class Event {
     @ManyToOne
     private Account createBy;
 
+    @Column(nullable = false)
     private String title;
 
     @Lob
     private String description;
 
+    @Column(nullable = false)
     private LocalDateTime createDateTime;
 
+    @Column(nullable = false)
     private LocalDateTime endEnrollmentDateTime;
 
+    @Column(nullable = false)
     private LocalDateTime startDateTime;
 
+    @Column(nullable = false)
     private LocalDateTime endDateTime;
 
     private int limitOfEnrollment;
+
+    private List<Enrollment> enrollments;
+
+    @Enumerated(EnumType.STRING)
+    private EventType eventType;
+
 }
---
+++ src/main/java/be/shwan/event/domain/EventType.java
@@ -0,0 +1,5 @@
+package be.shwan.event.domain;
+
+public enum EventType {
+    FCFS, CONFIRMATIVE;
+}
